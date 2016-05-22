abda75c133bc408a85cac054ca9621e48cbac842
fugehappy <fugehappy2009@163.com>
2016-05-22 14:25:54 +0800

    update alarm

--- alarm/main.js
+++ alarm/main.js
@@ -1,6 +1,33 @@
+'use strict';
 require([], function (require){
+	var date = new Date();
+	console.log(date);
+	console.log(typeof Goods);
+	console.log(typeof Promise);
+	console.log(typeof CustomEvent);
+	
+	//ES6(ECMAScript 6)
+	if(typeof Promise !='undefined'){
+		var promise = new Promise(function (resolve, reject){
+			//resolve("200");
+			reject("404");
+		}).then(function (result) {
+			console.log(result);
+		}).catch(function (reason) {
+			console.log(reason);
+		});
+	}
+	
+	
 	var db = require('alarm_database');
-	var alarm = require('alarm');
+	var stopwatch = require('stopwatch');
+	var s = new stopwatch();
+
+	var Alarm = require('alarm');
+	var alarm = new Alarm();
+	
+
 	db.withDatabase();
-	alarm.schedule();
+	alarm.isEnabled();
+	s.isEnabled();
 });
